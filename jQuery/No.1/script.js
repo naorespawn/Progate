147a1844fba84959c20cf7588737bace1011abd0
Naoki Egawa <nao.respawn@gmail.com>
2019-07-07 10:47:26 +0900

    jQuery No.1 part10

--- jQuery/No.1/script.js
+++ jQuery/No.1/script.js
@@ -1,6 +1,15 @@
 $(function() {
-    // 「.list-item」の要素に対するclickイベントを作成してください
-    $('.list-item').click(function(){
-      $(this).css('color','red');
+    $('.btn').click(function() {
+      // 「#title」のjQueryオブジェクトを変数$titleに代入してください
+      var $title = $('#title');
+      
+      
+      // 「#title」に対する3つのメソッドを、変数を使って書き換えてください
+      $title.css('color', 'red');
+      $title.html('こんばんは、にんじゃわんこさん');
+      $title.fadeOut(1000);
+      
+      // 「#text」に対する3つのメソッドを、メソッドチェーンを使って書き換えてください
+      $('#text').css('color', 'blue').html('<h3>jQueryをマスターしましょう！</h3>').fadeOut(1000);
     });
   });
