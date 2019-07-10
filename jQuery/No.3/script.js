f16f5ac6161da76e9a3abfd6467b4de63d6e5442
Naoki Egawa <nao.respawn@gmail.com>
2019-07-10 20:29:55 +0900

    jQuery No3 part15

--- jQuery/No.3/script.js
+++ jQuery/No.3/script.js
@@ -1,25 +1,17 @@
-$(function() {
-    $('#form').submit(function() {
-      var selectItem = $('#select-form').val();
-      var textItem = $('#text-form').val();
-      if (textItem == '') {
-        $('#error-message').text('理由を記入してください');
-      } else {
-        $('#error-message').text('');
-      }
-      $('#output-select').text(selectItem);
-      $('#output-text').text(textItem);
-      return false;
-    });
+$(function(){
   
-    $('.option-btn').click(function() {
-      var optionText = $(this).text();
-      // 変数clickedOptionに、クリックした要素のdata-optionの値を代入してください。
-      var clickedOption = $(this).attr('data-option');
-      
-      $('#text-form').val(optionText + 'が好きな理由は、');
-      // 変数clickedOptionを用いて、「#select-form」の値を自動で入力してください。
-      $('#select-form').val(clickedOption);
-      
-    });
+    // 「.social-icon」にマウスが乗ったときに、そのfont-sizeを30pxに変更し、
+    // 離れたときにはfont-sizeを24pxにしてください。
+    $('.social-icon').hover(
+      function(){
+        $(this).animate({
+          'font-size':'30px'
+        }, 300);
+      },
+      function(){
+        $(this).animate({
+          'font-size':'24px'
+        }, 300);
+      });
+  
   });
