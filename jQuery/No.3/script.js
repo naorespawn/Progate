b5c2ce31cba9eb7b4caa242c34fbdad14245adc1
Naoki Egawa <nao.respawn@gmail.com>
2019-07-10 19:33:08 +0900

    jQuery No3 part10

--- jQuery/No.3/script.js
+++ jQuery/No.3/script.js
@@ -1,11 +1,10 @@
 $(function() {
-    var title = $('#title').text();
-    // 2つの変数を定義してください
-    var id = $('.section-content p').attr('id');
-    var href = $('#link').attr('href');
-    
-    $('#title-text').text(title);
-    // textメソッドを用いて、それぞれ表示してください
-    $('#content-id').text(id);
-    $('#link-href').text(href);
+  
+    // 「#form」要素のsubmitイベントを作成してください
+    $('#form').submit(function(){
+      var textValue = $('#text-form').val();
+      $('#output-text').text(textValue);
+      return false;
+    });
+      
   });
