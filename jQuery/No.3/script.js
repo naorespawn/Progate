66b53fb2ef6910efa340e846b099739553ace973
Naoki Egawa <nao.respawn@gmail.com>
2019-07-10 19:08:22 +0900

    jQuery No3 part6

--- jQuery/No.3/script.js
+++ jQuery/No.3/script.js
@@ -1,22 +1,20 @@
 $(function() {
-    $('.index-btn').click(function() {
-      $('.active').removeClass('active');
-      var clickedIndex = $('.index-btn').index(this);
-      $('.slide').eq(clickedIndex).addClass('active');
-      
-      // 1. 変数slideIndexに「.active」要素のインデックス番号を代入してください
+    // toggleChangeBtn関数を定義してください
+    function toggleChangeBtn() {
       var slideIndex = $('.slide').index($('.active'));
-      
-      // 3. change-btn要素を表示してください
-     $('.change-btn').show();
-      
-      // 2. ifとelse ifを用いて、「.change-btn」要素の表示/非表示をおこなってください
-      if (slideIndex === 0){
+      $('.change-btn').show();
+      if (slideIndex == 0) {
         $('.prev-btn').hide();
-      }else if(slideIndex === 3){
+      } else if (slideIndex == 3) {
         $('.next-btn').hide();
       }
-      
+    }
+    
+    $('.index-btn').click(function() {
+      $('.active').removeClass('active');
+      var clickedIndex = $('.index-btn').index($(this));
+      $('.slide').eq(clickedIndex).addClass('active');
+      toggleChangeBtn();
     });
     
     $('.change-btn').click(function() {
@@ -28,20 +26,8 @@ $(function() {
         $displaySlide.prev().addClass('active');
       }
       
-      // index-btnのクリックイベントと同様の処理を記述してください
-      // 1. 変数slideIndexに「.active」要素のインデックス番号を代入してください
-      var slideIndex = $('.slide').index($('.active'));
-      
-      // 3. change-btn要素を表示してください
-     $('.change-btn').show();
-      
-      // 2. ifとelse ifを用いて、「.change-btn」要素の表示/非表示をおこなってください
-      if (slideIndex === 0){
-        $('.prev-btn').hide();
-      }else if(slideIndex === 3){
-        $('.next-btn').hide();
-      }
-      
+      // 以下をtoggleChangeBtn関数にまとめ、関数を呼び出すようにしてください
+      toggleChangeBtn();
     });
   });
   
