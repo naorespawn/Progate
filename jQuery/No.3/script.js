bced0d0e37f99f5707447ea88bee2f0286cd2337
Naoki Egawa <nao.respawn@gmail.com>
2019-07-10 19:13:01 +0900

    jQuery No3 part8

--- jQuery/No.3/script.js
+++ jQuery/No.3/script.js
@@ -1,32 +1,8 @@
 $(function() {
-  
-    function toggleChangeBtn() {
-      var slideIndex = $('.slide').index($('.active'));
-      $('.change-btn').show();
-      if (slideIndex == 0) {
-        $('.prev-btn').hide();
-      // 「3」の部分を、lengthメソッドを用いて書き換えてください
-      } else if (slideIndex == $('.slide').length - 1) {
-        $('.next-btn').hide();
-      }
-    }
+    // 変数titleに、「#title」要素のテキストを代入してください
+    var title = $('#title').text();
     
-    $('.index-btn').click(function() {
-      $('.active').removeClass('active');
-      var clickedIndex = $('.index-btn').index($(this));
-      $('.slide').eq(clickedIndex).addClass('active');
-      toggleChangeBtn();
-    });
+    // textメソッドを用いて、「#title-text」要素のテキストを書き換えてください
+    $('#title-text').text(title);
     
-    $('.change-btn').click(function() {
-      var $displaySlide = $('.active');
-      $displaySlide.removeClass('active');
-      if ($(this).hasClass('next-btn')) {
-        $displaySlide.next().addClass('active');
-      } else {
-        $displaySlide.prev().addClass('active');
-      }
-      toggleChangeBtn();
-    });
   });
-  
