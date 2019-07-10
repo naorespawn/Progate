bb58b6c04eb7e83bfb4685163bf58c949451fecf
Naoki Egawa <nao.respawn@gmail.com>
2019-07-10 20:05:38 +0900

    jQuery No3 part11

--- jQuery/No.3/script.js
+++ jQuery/No.3/script.js
@@ -1,10 +1,13 @@
 $(function() {
-  
-    // 「#form」要素のsubmitイベントを作成してください
-    $('#form').submit(function(){
+    $('#form').submit(function() {
+      // 変数selectValueを定義してください。
+      var selectValue = $('#select-form').val();
+      
       var textValue = $('#text-form').val();
+      // 「#output-select」要素のテキストを変数selectValueの値で書き換えてください。
+      $('#output-select').text(selectValue);
+      
       $('#output-text').text(textValue);
       return false;
     });
-      
   });
