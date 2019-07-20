5ffaa6e369b207389237554bcd4a26962da6e506
Naoki Egawa <nao.respawn@gmail.com>
2019-07-20 11:31:09 +0900

    ruby5 part6

--- Ruby/Ruby5/drink.rb
+++ Ruby/Ruby5/drink.rb
@@ -1,6 +1,5 @@
 require "./menu"
 
 class Drink < Menu
-  # amountというインスタンス変数を追加してください
   attr_accessor :amount
 end
--- Ruby/Ruby5/food.rb
+++ Ruby/Ruby5/food.rb
@@ -3,7 +3,6 @@ require "./menu"
 class Food < Menu
   attr_accessor :calorie
   
-  # calorie_infoメソッドを定義してください
   def calorie_info
     return "#{self.name}は#{self.calorie}kcalです"
   end
