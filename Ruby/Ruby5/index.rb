d06593e04144570dd39759a3c9ebc4f04f5e1f27
Naoki Egawa <nao.respawn@gmail.com>
2019-07-20 16:32:23 +0900

    ruby5 part13

--- Ruby/Ruby5/index.rb
+++ Ruby/Ruby5/index.rb
@@ -1,10 +1,27 @@
-require "date"
+class Menu
+  attr_accessor :name
+  attr_accessor :price
 
-# 変数todayに、Date.todayの戻り値を代入してください
-today = Date.today
-
-# 変数todayをputsしてください
-puts today
-
-# 変数todayに対してsunday?メソッドを用いた結果をputsしてください
-puts today.sunday?
+  def initialize(name:, price:)
+    self.name = name
+    self.price = price
+  end
+  
+  def info
+    return "#{self.name} #{self.price}円"
+  end
+  
+  def get_total_price(count)
+    total_price = self.price * count
+    if count >= 3
+      total_price -= 100
+    end
+    return total_price
+  end
+  
+  # クラスメソッド「is_discount_day?」を定義してください
+  def Menu.is_discount_day? 
+    
+  end
+  
+end
