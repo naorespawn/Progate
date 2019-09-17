27a651ec43f986dc88b34e00100dd8f09f74f8fe
Naoki Egawa <nao.respawn@gmail.com>
2019-09-17 20:08:38 +0900

    Users 作成

--- Ruby_on_Rails5/tweet_app/config/routes.rb
+++ Ruby_on_Rails5/tweet_app/config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/index'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  get 'posts/index'
+  get 'posts/index' => 'posts#index'
+  get 'posts/new' => 'posts#new'
+  post 'posts/create' => 'posts#create'
+  get 'posts/:id' => 'posts#show'
+  get 'posts/:id/edit' => 'posts#edit'
+  post 'posts/:id/update' => 'posts#update'
+  post 'posts/:id/destroy' => 'posts#destroy'
   get '/' => 'home#top'
   get 'about' => 'home#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
