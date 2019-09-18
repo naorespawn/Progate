d0b2e23ec2801993df91db639fd9fe4a0adb8bbc
Naoki Egawa <nao.respawn@gmail.com>
2019-09-18 20:53:14 +0900

    ユーザー作成ページを追加

--- Ruby_on_Rails5/tweet_app/config/routes.rb
+++ Ruby_on_Rails5/tweet_app/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/index'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
+  get '/' => 'home#top'
+  get 'about' => 'home#about'
+  
   get 'posts/index' => 'posts#index'
   get 'posts/new' => 'posts#new'
   post 'posts/create' => 'posts#create'
@@ -8,7 +11,14 @@ Rails.application.routes.draw do
   get 'posts/:id/edit' => 'posts#edit'
   post 'posts/:id/update' => 'posts#update'
   post 'posts/:id/destroy' => 'posts#destroy'
-  get '/' => 'home#top'
-  get 'about' => 'home#about'
+
+  get 'users/index' => 'users/index'
+  get 'users/:id' => 'users#show'
+  get 'signup' => 'users#new'
+  get 'users/:id/edit' => 'users#edit'
+  post 'users/create' => 'users#create'
+  post 'users/:id/update' => 'users#update'
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
