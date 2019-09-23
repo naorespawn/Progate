2794a3f9bec6a0070c43faea79b57f9c332338db
Naoki Egawa <nao.respawn@gmail.com>
2019-09-23 14:01:29 +0900

    passwordを暗号化

--- Ruby_on_Rails5/tweet_app/app/models/user.rb
+++ Ruby_on_Rails5/tweet_app/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   validates :name, {presence: true}
   validates :email, {presence: true, uniqueness: true}
-  validates :password, {presence: true}
+  has_secure_password
 
   def posts
     return Post.where(user_id: self.id)
